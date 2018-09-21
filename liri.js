e99edb70b24543f36f8feeffd54c1c689102a1c3
Matt Auretta <mauretta2@gmail.com>
2018-09-21 08:42:44 -0600

    added npm requires and switch statement

--- liri.js
+++ liri.js
@@ -1,3 +1,29 @@
+var keys = require("./keys.js");
+
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var request = require("request")
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+var moment = require('moment');
+moment().format();
+
+
+var command = process.argv[2];
+switch(command){
+    case "concert-this":
+    console.log("concert-this");
+    break;
+    case "spotify-this-song":
+    console.log("spotify-this-song");
+    break;
+    case "movie-this":
+    console.log("movie-this");
+    break;
+    case "do-what-it-says":
+    console.log("do-what-it-says");
+    break;
+}
