d8be5b0b2ed30a796ac29cc81fae4ffff12feac9
Matt Auretta <mauretta2@gmail.com>
2018-09-22 17:15:00 -0600

    bandsintown functional

--- liri.js
+++ liri.js
@@ -1,29 +1,66 @@
-var keys = require("./keys.js");
-
+//Require dotenv npm to link Spotify keys file
 require("dotenv").config();
 
+//Require keys.js file
+var keys = require("./keys.js");
+
+//Require request npm
 var request = require("request")
 
+//Require spotify npm
 var Spotify = require('node-spotify-api');
-
+//Save spotify key to a variable
 var spotify = new Spotify(keys.spotify);
 
+//Require moment npm
 var moment = require('moment');
 moment().format();
 
 
-var command = process.argv[2];
-switch(command){
+var nodeArgs = process.argv;
+var userInput = "";
+
+//Get user input for song/artist/movie name
+//Loop starting at process.argv[3]
+for (var i = 3; i < nodeArgs.length; i++) {
+    //If userInput is more than 1 word
+    if (i > 3 && i < nodeArgs.length) {
+        userInput = userInput + "%20" + nodeArgs[i];
+    }
+    //If userInput is only 1 word
+    else {
+        userInput += nodeArgs[i];
+    }
+}
+
+var userCommand = process.argv[2];
+switch (userCommand) {
     case "concert-this":
-    console.log("concert-this");
-    break;
+        //Run request to bandsintown with the specified artist
+        var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+        request(queryURL, function (error, response, body) {
+            //If no error and response is a success
+            if (!error && response.statusCode === 200) {
+                //Parse the json response
+                var data = JSON.parse(body);
+                for(var i = 0; i < data.length; i++){
+                    console.log("Venue: " + data[i].venue.name);
+                    if(data[i].venue.region == ""){
+                        data[i].venue.region = data[i].venue.country;
+                    }
+                    console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region);
+                    console.log("----------------")
+                }
+            }
+        });
+        break;
     case "spotify-this-song":
-    console.log("spotify-this-song");
-    break;
+        console.log("spotify-this-song");
+        break;
     case "movie-this":
-    console.log("movie-this");
-    break;
+        console.log("movie-this");
+        break;
     case "do-what-it-says":
-    console.log("do-what-it-says");
-    break;
+        console.log("do-what-it-says");
+        break;
 }
